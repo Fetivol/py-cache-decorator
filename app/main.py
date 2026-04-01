3bfb4989ddd05ac5a1ef0e5a56911c4a04dd90cb
Fetivol <Fetisowova@gmail.com>
2026-04-01 19:55:29 +0200

    developed cache function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_values = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+
+        if args not in cache_values:
+            cache_values[args] = func(*args, **kwargs)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+
+        return cache_values[args]
+
+    return wrapper

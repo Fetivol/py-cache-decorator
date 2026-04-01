d1c02da03a4d4ef51fb5355fb9fc8e6ca1778e75
Fetivol <Fetisowova@gmail.com>
2026-04-01 21:19:19 +0200

    update keys for args and kwargs in one tuple

--- app/main.py
+++ app/main.py
@@ -5,13 +5,15 @@ def cache(func: Callable) -> Callable:
     cache_values = {}
 
     def wrapper(*args, **kwargs) -> Any:
+        kwargs_key = tuple(sorted(kwargs.items()))
+        full_key = (args, kwargs_key)
 
-        if args not in cache_values:
-            cache_values[args] = func(*args, **kwargs)
+        if full_key not in cache_values:
+            cache_values[full_key] = func(*args, **kwargs)
             print("Calculating new result")
         else:
             print("Getting from cache")
 
-        return cache_values[args]
+        return cache_values[full_key]
 
     return wrapper
